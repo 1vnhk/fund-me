4825b2ca75fe52f9da328c90e2fc2a4a1c7b7436
1vnhk <iandrc9@gmail.com>
2026-02-21 02:16:47 +0100

    feat: add fund and tinyTip checks

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,9 +2,17 @@
 pragma solidity ^0.8.24;
 
 contract FundMe {
-    // 1. Get funds from users
-    function fund() public {}
+    function fund() public payable {
+        // Get funds from users
+        // Set a minimum funding value in USD
+
+        require(msg.value > 1 ether, "didn't send enough ETH");
+    }
+
     // 2. Withdraw funds to owner's account
     function withdraw() public {}
-    // 3. Set a minimum funding value in USD
+
+    function tinyTip() public payable {
+        require(msg.value < 1 gwei, "Please, send less than 1 Gwei");
+    }
 }
