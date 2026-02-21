9039c0f6c65a81e195e1d6b2336b336530840630
1vnhk <iandrc9@gmail.com>
2026-02-21 02:00:43 +0100

    feat: init FundMe

---
+++ contracts/FundMe.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract FundMe {
+    // 1. Get funds from users
+    function fund() public {}
+    // 2. Withdraw funds to owner's account
+    function withdraw() public {}
+    // 3. Set a minimum funding value in USD
+}
