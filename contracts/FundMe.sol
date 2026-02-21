73ea2eb2c53e9824e0269d3b2fd5059b4ecd0dc9
1vnhk <iandrc9@gmail.com>
2026-02-21 02:44:32 +0100

    feat: plan getting an amount in USD

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,17 +2,14 @@
 pragma solidity ^0.8.24;
 
 contract FundMe {
+    uint256 public minimumUSD = 5;
+
     function fund() public payable {
         // Get funds from users
         // Set a minimum funding value in USD
-
-        require(msg.value > 1 ether, "didn't send enough ETH");
+        require(msg.value >= minimumUSD, "didn't send enough ETH");
     }
 
     // 2. Withdraw funds to owner's account
     function withdraw() public {}
-
-    function tinyTip() public payable {
-        require(msg.value < 1 gwei, "Please, send less than 1 Gwei");
-    }
 }
