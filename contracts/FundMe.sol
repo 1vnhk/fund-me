79e6c6e42416a4970ab2aa0b8404a6bcf32a1322
1vnhk <iandrc9@gmail.com>
2026-02-23 12:01:01 +0100

    feat: move conversion to library

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,36 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import { PriceConverter } from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public constant MINIMUM_USD = 5 * (10 ** 18);
 
     address[] public funders;
     mapping(address funder => uint256 amount) public addressToFunded;
 
-    function fund() public payable returns (uint256) {
-        // Get funds from users
-        // Set a minimum funding value in USD
-        require(getConversionRate(msg.value) >= MINIMUM_USD, "didn't send enough ETH"); // msg.value has 18 decimals
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH"); // msg.value has 18 decimals
         funders.push(msg.sender);
         addressToFunded[msg.sender] += msg.value;
-    }
-
-    function getPrice() public view returns (uint256) {
-        // Address of the oracle ETH/USD on Sepolia: 0x694AA1769357215DE4FAC081bf1f309aDC325306 -- Chainlink
-        // ABI
-
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price) * (10 ** (18 - priceFeed.decimals()));
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) * (10 ** 18);
-
-        return ethAmountInUsd;
     }
 
     // 2. Withdraw funds to owner's account
