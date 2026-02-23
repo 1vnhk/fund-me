5f733d85c60fc7a93c1501b842b188507c7d78ae
1vnhk <iandrc9@gmail.com>
2026-02-23 12:36:23 +0100

    feat: add owner check for withdrawal

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -11,6 +11,12 @@ contract FundMe {
     address[] public funders;
     mapping(address funder => uint256 amount) public addressToFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH"); // msg.value has 18 decimals
         funders.push(msg.sender);
@@ -18,6 +24,8 @@ contract FundMe {
     }
 
     function withdraw() public {
+        require(msg.sender == owner, "Must be owner");
+
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToFunded[funder] = 0;
